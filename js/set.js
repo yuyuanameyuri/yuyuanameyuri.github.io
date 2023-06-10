358dc9114733c164154241c8339bd8cc3d9b9cac
ameyuri <81275420+yuyuanameyuri@users.noreply.github.com>
2023-06-11 00:21:36 +0800

    Update set.js

--- js/set.js
+++ js/set.js
@@ -41,10 +41,10 @@ function setBgImgInit() {
 
   switch (bg_img["type"]) {
     case "1":
-      $("#bg").attr(
-        "src",
-        `./img/background${1 + ~~(Math.random() * 10)}.webp`
-      ); //随机默认壁纸
+    //   $("#bg").attr(
+    //     "src",
+    //     `./img/background${1 + ~~(Math.random() * 10)}.webp`
+    //   ); //随机默认壁纸
       break;
     case "2":
       $("#bg").attr("src", bg_img_preinstall[2]); //必应每日
